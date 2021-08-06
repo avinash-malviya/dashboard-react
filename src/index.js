8be0c35d5f10f47a74f153fe8ceac67dc21ffa10
avinash-malviya <avinashmalviya500@gmail.com>
2021-08-06 21:41:45 +0530

    last

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
-
 import { createStore } from "redux";
-
 import { Provider } from "react-redux";
-
-import rootReducer from "./redux/reducers";
 import { ChakraProvider } from "@chakra-ui/react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
+import rootReducer from "./redux/reducers";
+
 import "./assets/boxicons-2.0.7/css/boxicons.min.css";
 import "./assets/css/grid.css";
+import "./assets/css/theme.css";
 import "./assets/css/index.css";
 
 import Layout from "./components/layout/Layout";
 
 const store = createStore(rootReducer);
 
-document.title = "Tua CRM";
+document.title = "Platform3Solutions";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -34,7 +33,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
