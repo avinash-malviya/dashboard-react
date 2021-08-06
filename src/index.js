2736a79a764417940a9611cd43180c777d659505
avinash-malviya <avinashmalviya500@gmail.com>
2021-08-06 20:44:19 +0530

    final

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+
+import { createStore } from "redux";
+
+import { Provider } from "react-redux";
+
+import rootReducer from "./redux/reducers";
+import { ChakraProvider } from "@chakra-ui/react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+import "./assets/boxicons-2.0.7/css/boxicons.min.css";
+import "./assets/css/grid.css";
+import "./assets/css/index.css";
+
+import Layout from "./components/layout/Layout";
+
+const store = createStore(rootReducer);
+
+document.title = "Tua CRM";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <DndProvider backend={HTML5Backend}>
+        <ChakraProvider>
+          <Layout />
+        </ChakraProvider>
+      </DndProvider>
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
